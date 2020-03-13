fe5a1504aad9e142e3161edd8d21f9f81e2c1f04
Amr Ezzat <3mr3zzat@gmail.com>
2020-03-13 13:20:18 +0200

    [Database] Add User Licenses Table

---
+++ database/migrations/2020_03_10_195847_add_default_license_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDefaultLicenseToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('default_license')->nullable()->after('country_id');
+            $table->foreign('default_license')->references('id')->on('diving_courses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_default_license_foreign');
+            $table->dropColumn('default_license');
+        });
+    }
+}
