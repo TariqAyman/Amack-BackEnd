df54e9044c5f09f54b930f723e8b2c4b05c56f5e
Tariq Ayman <tariq.ayman94@gmail.com>
2020-11-09 10:02:32 +0200

    [ API ] add SiteResource.php

---
+++ app/Http/Resources/SiteResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SiteResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "name" => $this->name,
+            "images" => $this->images->pluck('path'),
+            "site_type" => $this->mainTaxon->name,
+            "max_depth" => $this->max_depth,
+            "visibility" => $this->visibility,
+            "required_license" => "",
+            "time_of_day" => DayTime::collection($this->dayTimes),
+            "activities" => DiveActivity::collection($this->activities),
+            "when_to_visit" => [],
+            "site_features" => Taxon::collection($this->subTaxons),
+            "recommended_equipment" => EquipmentResource::collection($this->equipments),
+            "near_sites" => NearbySiteResource::collection($this->nearbySites),
+            "rate" => $this->rate,
+            "isSpecial" => $this->special,
+            "isAvailable" => $this->enabled
+        ];
+    }
+}
