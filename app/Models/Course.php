78c8f924cdbdb796f9a3cb85044e13bec8a9be5b
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:55:47 +0200

    [ Model ] improve Course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,12 +4,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Course extends Model
 {
+    use LogsActivity , SoftDeletes;
+
     protected $table = 'diving_courses';
 
     protected $fillable = [
+        'name', 'description', 'school_id', 'license_type', 'required_license_id', 'learning_type', 'days_num','enabled'
+    ];
+
+    protected static $logName = 'diving_courses';
+    protected static $logAttributes = [
         'name', 'description', 'school_id', 'license_type', 'required_license_id', 'learning_type', 'days_num','enabled'
     ];
 
@@ -22,4 +31,6 @@ class Course extends Model
     {
         return $this->belongsTo(self::class, 'required_license_id', 'id');
     }
+
+    // todo: number of dives
 }
