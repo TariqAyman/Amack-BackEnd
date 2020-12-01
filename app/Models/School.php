8ff750a029988e7604a66d5797a0badd0a0ab8c1
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:57:43 +0200

    [ Model ] improve School model

--- app/Models/School.php
+++ app/Models/School.php
@@ -4,13 +4,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class School extends Model
 {
+    use LogsActivity, SoftDeletes;
+
     protected $table = 'diving_schools';
 
-    protected $fillable = ['name', 'logo','enabled'];
+    protected $fillable = ['name', 'logo', 'enabled'];
+
+    protected static $logName = 'diving_schools';
+
+    protected static $logAttributes = ['name', 'logo', 'enabled'];
 
     public function courses()
     {
