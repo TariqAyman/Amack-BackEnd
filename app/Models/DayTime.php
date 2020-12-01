74be792002eb3be780a3012486b9f2b1d4855b22
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:56:16 +0200

    [ Model ] improve DayTime model

--- app/Models/DayTime.php
+++ app/Models/DayTime.php
@@ -5,12 +5,21 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class DayTime extends Model
 {
+    use LogsActivity, SoftDeletes;
+
     protected $table = 'day_times';
     protected $fillable = ['name', 'icon'];
+
+    protected static $logName = 'day_times';
+    protected static $logAttributes = [
+        'name', 'icon'
+    ];
 
     public function getIconAttribute($photo)
     {
