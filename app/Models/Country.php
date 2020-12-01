c2aaf385b83abc24c1771b60898d6c07fb6e23c4
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:55:11 +0200

    [ Model ] improve Country model

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,12 +4,24 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Country extends Model
 {
+    use LogsActivity , SoftDeletes;
+
     protected $table = 'countries';
 
-    protected $fillable = ['name', 'enabled_for_dive','enabled_for_signup'];
+    protected $fillable = [
+        'name', 'enabled_for_dive', 'enabled_for_signup', 'code', 'phone_code', 'latitude', 'longitude'
+    ];
+
+    protected static $logName = 'countries';
+
+    protected static $logAttributes = [
+        'name', 'enabled_for_dive', 'enabled_for_signup', 'code', 'phone_code', 'latitude', 'longitude'
+    ];
 
     public function cities()
     {
