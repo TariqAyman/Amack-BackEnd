cfdd226602ad7d229fa12a75fad07d551902e09c
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:57:54 +0200

    [ Model ] improve Season model

--- app/Models/Season.php
+++ app/Models/Season.php
@@ -5,13 +5,25 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Season extends Model
 {
+    use LogsActivity, SoftDeletes;
+
     protected $table = 'seasons';
 
-    protected $fillable = ['name','icon'];
+    protected $fillable = [
+        'name', 'icon'
+    ];
+
+    protected static $logName = 'seasons';
+
+    protected static $logAttributes = [
+        'name', 'icon'
+    ];
 
     public function getIconAttribute($photo)
     {
