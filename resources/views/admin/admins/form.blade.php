eafc6ed37cb9f75eb7e07a96b0d34a22e426b5f7
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:48:35 +0200

    [ Admins ] edit CRUD

--- resources/views/admin/admins/form.blade.php
+++ resources/views/admin/admins/form.blade.php
@@ -37,42 +37,43 @@
                             @endif
                             <div class="card-body">
                                 @csrf
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
-                                           class="form-control" id="name"
-                                           placeholder="name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="mobile">Mobile</label>
-                                    <input @if(isset($data)) value="{{$data->mobile}}" @endif name="mobile" type="text"
-                                           class="form-control" id="mobile"
-                                           placeholder="mobile">
-                                </div>
-                                <div class="form-group">
-                                    <label for="email">Email address</label>
-                                    <input required @if(isset($data)) value="{{$data->email}}" @endif name="email"
-                                           type="email"
-                                           class="form-control" id="email"
-                                           placeholder="Enter email">
-                                </div>
-                                <div class="form-group">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input name="password" type="password" class="form-control"
-                                           id="exampleInputPassword1"
-                                           placeholder="Password">
-                                </div>
-                                <div class="form-group">
-                                    <label for="photo">Avatar</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input name="photo" type="file"
-                                                   id="photo">
-                                        </div>
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="name">Name</label>
+                                        <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
+                                               class="form-control" id="name"
+                                               placeholder="name">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="mobile">Mobile</label>
+                                        <input @if(isset($data)) value="{{$data->mobile}}" @endif name="mobile" type="text"
+                                               class="form-control" id="mobile"
+                                               placeholder="mobile">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="email">Email address</label>
+                                        <input required @if(isset($data)) value="{{$data->email}}" @endif name="email"
+                                               type="email"
+                                               class="form-control" id="email"
+                                               placeholder="Enter email">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input name="password" type="password" class="form-control"
+                                               id="exampleInputPassword1"
+                                               placeholder="Password">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="photo">Avatar</label>
+                                        <div class="input-group">
+                                            <div class="custom-file">
+                                                <input name="photo" type="file"
+                                                       id="photo">
+                                            </div>
 
+                                        </div>
                                     </div>
                                 </div>
-
                             </div>
                             <!-- /.card-body -->
 
