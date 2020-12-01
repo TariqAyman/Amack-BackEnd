0e70f4e714a0f6a96838d5ec5cbdc32206ebc3a5
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:50:37 +0200

    [ Courses ] edit CRUD

--- resources/views/admin/courses/form.blade.php
+++ resources/views/admin/courses/form.blade.php
@@ -37,75 +37,77 @@
                             @endif
                             <div class="card-body">
                                 @csrf
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
-                                           class="form-control" id="name"
-                                           placeholder="name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="description">Description</label>
-                                    <textarea name="description"
-                                              class="form-control" id="description"
-                                              placeholder="Description"> @if(isset($data)){{$data->description}} @endif</textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="days_num">Days</label>
-                                    <input @if(isset($data)) value="{{$data->days_num}}" @endif name="days_num"
-                                           type="number"
-                                           class="form-control" id="days_num" min="0"
-                                           placeholder="days_num">
-                                </div>
-                                <div class="form-group">
-                                    <label for="license_type">License Type</label>
-                                    <select class="custom-select" required id="license_type" name="license_type">
-                                        <option @if(isset($data) && $data->license_type === 'specialty') selected
-                                                @endif value="specialty"> Specialty
-                                        </option>
-                                        <option @if(isset($data) && $data->license_type === 'main') selected
-                                                @endif value="main"> Main
-                                        </option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="learning_type">Learning Type</label>
-                                    <select class="custom-select" required id="learning_type" name="learning_type">
-                                        <option @if(isset($data) && $data->learning_type === 'theoretical') selected
-                                                @endif value="theoretical"> Theoretical
-                                        </option>
-                                        <option @if(isset($data) && $data->learning_type === 'practical') selected
-                                                @endif value="practical"> Practical
-                                        </option>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="school_id">School</label>
-                                    <select class="custom-select" required id="school_id" name="school_id">
-                                        @foreach($schools as $school)
-                                            <option
-                                                @if(isset($data) && $data->school_id === $school->id) selected
-                                                @endif value="{{$school->id}}"> {{$school->name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="required_license_id">Required License</label>
-                                    <select class="custom-select" required id="required_license_id"
-                                            name="required_license_id">
-                                        @foreach($licenses as $license)
-                                            <option
-                                                @if(isset($data) && $data->required_license_id === $license->id) selected
-                                                @endif value="{{$license->id}}"> {{$license->name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <label for="enabled">Enabled</label>
-                                    <div class="bootstrap-switch-square">
-                                        <input type="hidden" name="enabled" value="0">
-                                        <input type="checkbox" data-toggle="toggle" data-width="100" name="enabled"
-                                               id="enabled" @if(isset($data) && $data->enabled) checked
-                                               @endif value="1"/>
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="name">Name</label>
+                                        <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
+                                               class="form-control" id="name"
+                                               placeholder="name">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="description">Description</label>
+                                        <textarea name="description"
+                                                  class="form-control" id="description"
+                                                  placeholder="Description"> @if(isset($data)){{$data->description}} @endif</textarea>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="days_num">Days</label>
+                                        <input @if(isset($data)) value="{{$data->days_num}}" @endif name="days_num"
+                                               type="number"
+                                               class="form-control" id="days_num" min="0"
+                                               placeholder="days_num">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="license_type">License Type</label>
+                                        <select class="custom-select" required id="license_type" name="license_type">
+                                            <option @if(isset($data) && $data->license_type === 'specialty') selected
+                                                    @endif value="specialty"> Specialty
+                                            </option>
+                                            <option @if(isset($data) && $data->license_type === 'main') selected
+                                                    @endif value="main"> Main
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="learning_type">Learning Type</label>
+                                        <select class="custom-select" required id="learning_type" name="learning_type">
+                                            <option @if(isset($data) && $data->learning_type === 'theoretical') selected
+                                                    @endif value="theoretical"> Theoretical
+                                            </option>
+                                            <option @if(isset($data) && $data->learning_type === 'practical') selected
+                                                    @endif value="practical"> Practical
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="school_id">School</label>
+                                        <select class="custom-select" required id="school_id" name="school_id">
+                                            @foreach($schools as $school)
+                                                <option
+                                                    @if(isset($data) && $data->school_id === $school->id) selected
+                                                    @endif value="{{$school->id}}"> {{$school->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="required_license_id">Required License</label>
+                                        <select class="custom-select" required id="required_license_id"
+                                                name="required_license_id">
+                                            @foreach($licenses as $license)
+                                                <option
+                                                    @if(isset($data) && $data->required_license_id === $license->id) selected
+                                                    @endif value="{{$license->id}}"> {{$license->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="enabled">Enabled</label>
+                                        <div class="bootstrap-switch-square">
+                                            <input type="hidden" name="enabled" value="0">
+                                            <input type="checkbox" data-toggle="toggle" data-width="100" name="enabled"
+                                                   id="enabled" @if(isset($data) && $data->enabled) checked
+                                                   @endif value="1"/>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
