32cf1f02e8de76101ca6b297c667c20e59b7d333
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:56:04 +0200

    [ Model ] improve Admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,12 +4,44 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Admin extends Authenticatable
 {
+    use Notifiable, LogsActivity, SoftDeletes;
+
     protected $table = 'admins';
 
-    protected $fillable = ['email', 'password', 'name', 'photo'];
-    protected $hidden = ['password'];
+    protected $guard = 'admin';
+    protected $guard_name = 'admin';
+
+    protected $fillable = ['email', 'password', 'name', 'photo','gender'];
+
+    protected static $logName = 'admins';
+    protected static $logAttributes = ['name', 'email', 'photo'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+
+    protected $dates = ['create_at', 'updated_at','email_verified_at'];
+
+    public function setPasswordAttribute($value)
+    {
+        if (!empty($value)) {
+            return $this->attributes['password'] = Hash::make($value);
+        }
+    }
+
+    // todo: title , permissions & role
 }
