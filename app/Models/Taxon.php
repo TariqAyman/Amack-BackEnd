87d74ad7141e5899156e4df87b6a5cf79fcf2fed
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:58:29 +0200

    [ Model ] improve Taxon model

--- app/Models/Taxon.php
+++ app/Models/Taxon.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Taxon extends Model
 {
+    use LogsActivity, SoftDeletes;
+
     protected $table = 'taxons';
 
     protected $fillable = ['name', 'description', 'photo'];
+
+    protected static $logName = 'taxons';
+
+    protected static $logAttributes = [
+        'name', 'description', 'photo'
+    ];
 
     public function getPhotoAttribute($photo)
     {
