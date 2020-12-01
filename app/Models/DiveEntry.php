f142c159bb8fae0dd52b7bc5e2c91ec6de7eb195
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:56:38 +0200

    [ Model ] improve DiveEntry model

--- app/Models/DiveEntry.php
+++ app/Models/DiveEntry.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class DiveEntry extends Model
 {
+    use LogsActivity, SoftDeletes;
+
     protected $table = 'dive_entries';
 
     protected $fillable = ['name', 'photo'];
+
+    protected static $logName = 'dive_entries';
+    protected static $logAttributes = [
+        'name', 'photo'
+    ];
+
 
     public function getPhotoAttribute($photo)
     {
