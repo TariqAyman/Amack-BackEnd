36b1493dbf3bb95d93bf29f481d89673edbe64d8
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-01 07:56:27 +0200

    [ Model ] improve DiveActivity model

--- app/Models/DiveActivity.php
+++ app/Models/DiveActivity.php
@@ -5,13 +5,22 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Storage;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class DiveActivity extends Model
 {
+    use LogsActivity , SoftDeletes;
+
     protected $table = 'dive_activities';
 
     protected $fillable = ['name', 'description','icon'];
+
+    protected static $logName = 'dive_activities';
+    protected static $logAttributes = [
+        'name', 'description','icon'
+    ];
 
     public function getIconAttribute($photo)
     {
