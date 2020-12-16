0f9e7104e9a5806ec5077d01e14d1c32f48fb4b7
Tariq Ayman <tariq.ayman94@gmail.com>
2020-12-17 00:26:18 +0200

    [ City ] edit city images

--- app/Http/Resources/CitiesResource.php
+++ app/Http/Resources/CitiesResource.php
@@ -17,7 +17,7 @@ class CitiesResource extends JsonResource
         return [
             "id" => $this->id,
             "name" => $this->name,
-            "image" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTItKb-XnJWhU_NkWbaijbwom30tWK11Q6teg&usqp=CAU",
+            "images" => collect($this->images)->merge(["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTItKb-XnJWhU_NkWbaijbwom30tWK11Q6teg&usqp=CAU"]) ,
             "rate" => 3.5,
             "dive_sites" => $this->sites->count(),
             "location" => [
