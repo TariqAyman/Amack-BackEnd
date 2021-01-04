a2ce23efa1a0e1d7f6b0fe8876205743ec5a5fa8
Tariq Ayman <tariq.ayman94@gmail.com>
2021-01-04 10:12:15 +0200

    [ Seeder ] edit equipments seeder

--- database/seeds/EquipmentsTableSeeder.php
+++ database/seeds/EquipmentsTableSeeder.php
@@ -17,12 +17,12 @@ class EquipmentsTableSeeder extends Seeder
 
         DB::table('equipments')->delete();
         $equipments = [
-            ['id' => 1, 'name' => "Weight Belt",'night_dive'=>0,'nitrox'=>0,'state'=>'Main','season_id'=>Season::all()->random()->id,],
-            ['id' => 2, 'name' => "Boots",'night_dive'=>0,'nitrox'=>0,'state'=>'Main','season_id'=>Season::all()->random()->id,],
-            ['id' => 3, 'name' => "Suit",'night_dive'=>0,'nitrox'=>0,'state'=>'Main','season_id'=>Season::all()->random()->id,],
-            ['id' => 4, 'name' => "BCD",'night_dive'=>0,'nitrox'=>0,'state'=>'Main','season_id'=>Season::all()->random()->id,],
-            ['id' => 5, 'name' => "Fins",'night_dive'=>0,'nitrox'=>0,'state'=>'Main','season_id'=>Season::all()->random()->id,],
-            ['id' => 6, 'name' => "Mask",'night_dive'=>0,'nitrox'=>0,'state'=>'Main','season_id'=>Season::all()->random()->id,],
+            ['id' => 1, 'name' => "Weight Belt", 'night_dive' => 0, 'nitrox' => 0, 'state' => 'main', 'season_id' => Season::all()->random()->id,],
+            ['id' => 2, 'name' => "Boots", 'night_dive' => 0, 'nitrox' => 0, 'state' => 'main', 'season_id' => Season::all()->random()->id,],
+            ['id' => 3, 'name' => "Suit", 'night_dive' => 0, 'nitrox' => 0, 'state' => 'main', 'season_id' => Season::all()->random()->id,],
+            ['id' => 4, 'name' => "BCD", 'night_dive' => 0, 'nitrox' => 0, 'state' => 'main', 'season_id' => Season::all()->random()->id,],
+            ['id' => 5, 'name' => "Fins", 'night_dive' => 0, 'nitrox' => 0, 'state' => 'main', 'season_id' => Season::all()->random()->id,],
+            ['id' => 6, 'name' => "Mask", 'night_dive' => 0, 'nitrox' => 0, 'state' => 'main', 'season_id' => Season::all()->random()->id,],
         ];
         DB::table('equipments')->insert($equipments);
     }
