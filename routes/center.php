4195f2b1a5f09316e4c8978659741a9a72fb06a3
Tariq Ayman <tariq.ayman94@gmail.com>
2021-01-04 21:10:23 +0200

    [ Routes ] create center routes

---
+++ routes/center.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * Developer: Tariq Ayman ( tariq.ayman94@gmail.com )
+ * Date: 12/2/20, 3:42 AM
+ * Last Modified: 12/2/20, 3:42 AM
+ * Project Name: Amack-BackEnd
+ * File Name: center.php
+ */
+
+// Auth routes
+Route::get('/', 'Auth\AuthController@showLoginForm');
+Route::get('login', 'Auth\AuthController@showLoginForm')->name('center.login');
+Route::post('login', 'Auth\AuthController@login')->name('center.login.post');
+Route::get('/forgot-password', 'Auth\AuthController@showLoginForm')->name('center.forgot-password');
+
+Route::name('center.')->middleware('auth:staff')->group(function () {
+    // Auth routes
+    Route::get('logout', 'Auth\AuthController@logout')->name('center.logout');
+
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+
+    Route::get('/info', 'CenterController@index')->name('info');
+    Route::post('/info', 'CenterController@store')->name('info.post');
+
+    Route::view('promotions', 'center.promotions')->name('promotions');
+    Route::view('statistics', 'center.statistics')->name('statistics');
+
+    Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+    Route::get('site/{id}', 'SitesController@show');
+});
