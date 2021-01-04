76ca7275b14a9e352ccf1d85ae191c19d13a9075
Tariq Ayman <tariq.ayman94@gmail.com>
2021-01-04 10:17:27 +0200

    [ Model ] create Staff

---
+++ app/Models/Staff.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models;
+
+use App\Notifications\ResetPassword as ResetPasswordNotification;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Staff extends Authenticatable
+{
+    use HasFactory, LogsActivity, SoftDeletes, HasApiTokens, Notifiable;
+
+    protected $table = 'staff';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password', 'mobile', 'photo', 'gender', 'center_id'
+    ];
+
+    protected static $logName = 'staff';
+
+    protected static $logAttributes = [
+        'name', 'email', 'password', 'mobile', 'photo', 'gender', 'center_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        if (!empty($value)) {
+            return $this->attributes['password'] = Hash::make($value);
+        }
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+
+    public function center()
+    {
+        return $this->belongsTo(Center::class, 'center_id');
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        return Storage::url($value);
+    }
+}
