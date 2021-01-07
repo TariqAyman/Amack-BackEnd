1cb26b6a993d39e49dc04dd4a95fd05552a228b0
Tariq Ayman <tariq.ayman94@gmail.com>
2021-01-07 23:07:12 +0200

    [ Handler ] fix redirect to login page

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -72,7 +72,7 @@ class Handler extends ExceptionHandler
                 $login = 'center.login';
                 break;
             default:
-                $login = 'home';
+                $login = 'login';
                 break;
         }
 
