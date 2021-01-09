2c716a0a46e2feb8fcd166db3068ab5d715f89a9
Tariq Ayman <tariq.ayman94@gmail.com>
2021-01-09 23:44:36 +0200

    edit map modal for admin

--- resources/views/admin/partials/map.blade.php
+++ resources/views/admin/partials/map.blade.php
@@ -1,3 +1,91 @@
+<style type="text/css">
+    /* Always set the map height explicitly to define the size of the div
+     * element that contains the map. */
+    #dvMap {
+        height: 100%;
+    }
+
+    /* Optional: Makes the sample page fill the window. */
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+
+    #description {
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+    }
+
+    #infowindow-content .title {
+        font-weight: bold;
+    }
+
+    #infowindow-content {
+        display: none;
+    }
+
+    #dvMap #infowindow-content {
+        display: block;
+    }
+
+    .pac-card {
+        margin: 10px 10px 0 0;
+        border-radius: 2px 0 0 2px;
+        box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        outline: none;
+        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
+        background-color: #fff;
+        font-family: Roboto;
+    }
+
+    #pac-container {
+        padding-bottom: 12px;
+        margin-right: 12px;
+        z-index: 10000 !important;
+    }
+
+    .pac-controls {
+        display: inline-block;
+        padding: 5px 11px;
+    }
+
+    .pac-controls label {
+        font-family: Roboto;
+        font-size: 13px;
+        font-weight: 300;
+    }
+
+    #search-input {
+        background-color: #fff;
+        font-family: Roboto;
+        font-size: 15px;
+        font-weight: 300;
+        margin-left: 12px;
+        padding: 0 11px 0 13px;
+        text-overflow: ellipsis;
+        width: 400px;
+    }
+
+    #search-input:focus {
+        border-color: #4d90fe;
+    }
+
+    #title {
+        color: #fff;
+        background-color: #4d90fe;
+        font-size: 25px;
+        font-weight: 500;
+        padding: 6px 12px;
+    }
+
+    #target {
+        width: 345px;
+    }
+</style>
 <div class="modal fade" id="modal-map">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
@@ -8,8 +96,12 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div id="dvMap" style="width: 750px; height: 500px">
+                <div class="form-group col-md-12">
+                    <label for="search-input"></label>
+                    <input id="search-input" class="form-control" type="text" placeholder="Search Box"/>
                 </div>
+
+                <div id="dvMap" style="width: 750px; height: 500px"></div>
             </div>
             <div class="modal-footer justify-content-between">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -19,12 +111,18 @@
     </div>
     <!-- /.modal-dialog -->
 </div>
-
-<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&libraries=place"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAP_API_KEY') }}&callback=initAutocomplete&libraries=places&v=weekly" defer></script>
 <script type="text/javascript">
 
-    $(document).ready(function () {
+    var pacContainerInitialized = false;
+    $('#search-input').keypress(function() {
+        if (!pacContainerInitialized) {
+            $('.pac-container').css('z-index', '9999');
+            pacContainerInitialized = true;
+        }
+    });
 
+    function initAutocomplete() {
         var latLng = new google.maps.LatLng({{ $lat ?? 27.342298 }},{{ $lng ?? 33.576423 }});
 
         var map = new google.maps.Map(document.getElementById('dvMap'), {
@@ -43,8 +141,65 @@
         google.maps.event.addListener(marker, 'dragend', function (event) {
             document.getElementById("latitude").value = this.getPosition().lat();
             document.getElementById("longitude").value = this.getPosition().lng();
-            console.log(this.getPosition().lat(),this.getPosition().lng())
+            console.log(this.getPosition().lat(), this.getPosition().lng())
         });
-    });
+
+        // Create the search box and link it to the UI element.
+        const input = document.getElementById("search-input");
+        const searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener("bounds_changed", () => {
+            searchBox.setBounds(map.getBounds());
+        });
+        let markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener("places_changed", () => {
+            const places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+            // Clear out the old markers.
+            markers.forEach((marker) => {
+                marker.setMap(null);
+            });
+            markers = [];
+            // For each place, get the icon, name and location.
+            const bounds = new google.maps.LatLngBounds();
+            places.forEach((place) => {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                const icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25),
+                };
+                // Create a marker for each place.
+                markers.push(
+                    // new google.maps.Marker({
+                    //     map,
+                    //     icon,
+                    //     title: place.name,
+                    //     position: place.geometry.location,
+                    // })
+                    marker
+                );
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.fitBounds(bounds);
+        });
+    }
 
 </script>
