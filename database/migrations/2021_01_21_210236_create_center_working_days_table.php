521ad0813fa70b13d9ede1ec8598731fb05c2eef
Tariq Ayman <tariq.ayman94@gmail.com>
2021-01-28 06:31:50 +0200

    [ Migration ] create center_working_days table

---
+++ database/migrations/2021_01_21_210236_create_center_working_days_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCenterWorkingDaysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('center_working_days', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('center_id');
+            $table->foreign('center_id')->references('id')->on('centers')->onDelete('cascade');
+            $table->json('sunday');
+            $table->json('monday');
+            $table->json('tuesday');
+            $table->json('wednesday');
+            $table->json('thursday');
+            $table->json('friday');
+            $table->json('saturday');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('center_working_days');
+    }
+}
