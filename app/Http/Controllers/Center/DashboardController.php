95e87f0aa38c0fb04afbc06adfd5b0f57fae9ffa
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-01 02:13:21 +0200

    add Equipment in dashboard view to use in modal

--- app/Http/Controllers/Center/DashboardController.php
+++ app/Http/Controllers/Center/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Center;
 use App\Http\Controllers\Controller;
 use App\Repositories\CityRepository;
 use App\Repositories\DiveSiteRepository;
+use App\Repositories\EquipmentRepository;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -17,17 +18,23 @@ class DashboardController extends Controller
      * @var DiveSiteRepository
      */
     private $diveSiteRepository;
+    /**
+     * @var EquipmentRepository
+     */
+    private $equipmentRepository;
 
     /**
      * DashboardController constructor.
      * @
      * @param CityRepository $cityRepository
      * @param DiveSiteRepository $diveSiteRepository
+     * @param EquipmentRepository $equipmentRepository
      */
-    public function __construct(CityRepository $cityRepository, DiveSiteRepository $diveSiteRepository)
+    public function __construct(CityRepository $cityRepository, DiveSiteRepository $diveSiteRepository, EquipmentRepository $equipmentRepository)
     {
         $this->cityRepository = $cityRepository;
         $this->diveSiteRepository = $diveSiteRepository;
+        $this->equipmentRepository = $equipmentRepository;
     }
 
     /**
@@ -43,8 +50,9 @@ class DashboardController extends Controller
         $cities = $this->cityRepository->getAllWith(['sites']);
         $info = auth()->user()->center;
         $sites = $this->diveSiteRepository->mySites()->pluck('name', 'id');
+        $equipments = $this->equipmentRepository->findAll()->pluck('name', 'id');
 
-        return view('center.dashboard', compact('breadcrumbs', 'cities', 'info', 'sites'));
+        return view('center.dashboard', compact('breadcrumbs', 'cities', 'info', 'sites', 'equipments'));
     }
 
     /**
