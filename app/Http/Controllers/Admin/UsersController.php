0bf76b01868c25ea0986211dd23bd8287993e4ab
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 08:09:48 +0200

    edit UsersController for use breadcrumbs

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -10,4 +10,7 @@ class UsersController extends AdminController
 {
     protected $repository = UserRepository::class;
     protected $block = 'users';
+
+    protected $indexBreadcrumbs = [['link' => "admin/dashboard", 'name' => "Dashboard"], ['link' => 'admin/users', 'name' => "Users"]];
+
 }
