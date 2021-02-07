5ea3a24aa8e990b810c6e988e64e245df0fc1c8f
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 08:02:01 +0200

    edit new template : User view { list , edit & create }

--- resources/views/admin/users/form.blade.php
+++ resources/views/admin/users/form.blade.php
@@ -1,89 +1,80 @@
-@extends('admin.layouts.master')
+@extends('admin/layouts/contentLayoutMaster')
+
+@section('title', isset($data)? 'Edit User' : 'New User')
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{isset($data)? 'Edit User '.$data->id : 'New User'}}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('users.index')}}">Users</a></li>
-                        <li class="breadcrumb-item active">{{isset($data)? 'Edit User' : 'New User'}}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-primary">
-                        @if(session('status'))
-                            <div class="alert alert-success alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-                                </button>
-                                <h5><i class="icon fas fa-check"></i> Success!</h5>
-                                {{ session('status') }}
-                            </div>
-                        @endif
+    <section id="multiple-column-form">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{isset($data)? 'Edit User '.$data->id : 'New User'}}</h4>
+                    </div>
+                    <div class="card-body">
                         <form method="post" enctype="multipart/form-data"
                               action="{{isset($data)? route('users.update',$data->id):route('users.store')}}"
                               role="form">
                             @if(isset($data))
                                 @method('PUT')
                             @endif
-                            <div class="card-body">
-                                @csrf
-                                <div class="form-group col-md-6">
-                                    <label for="name">Name</label>
-                                    <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
-                                           class="form-control" id="name"
-                                           placeholder="name">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="mobile">Mobile</label>
-                                    <input @if(isset($data)) value="{{$data->mobile}}" @endif name="mobile" type="text"
-                                           class="form-control" id="mobile"
-                                           placeholder="mobile">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="email">Email address</label>
-                                    <input required @if(isset($data)) value="{{$data->email}}" @endif name="email"
-                                           type="email"
-                                           class="form-control" id="email"
-                                           placeholder="Enter email">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="exampleInputPassword1">Password</label>
-                                    <input name="password" type="password" class="form-control"
-                                           id="exampleInputPassword1"
-                                           placeholder="Password">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="photo">Avatar</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input name="photo" type="file"
-                                                   id="photo">
-                                        </div>
 
+                            @csrf
+                            <div class="form-group col-md-6">
+                                <label for="name">Name</label>
+                                <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
+                                       class="form-control" id="name"
+                                       placeholder="name">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="mobile">Mobile</label>
+                                <input @if(isset($data)) value="{{$data->mobile}}" @endif name="mobile" type="text"
+                                       class="form-control" id="mobile"
+                                       placeholder="mobile">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="email">Email address</label>
+                                <input required @if(isset($data)) value="{{$data->email}}" @endif name="email"
+                                       type="email"
+                                       class="form-control" id="email"
+                                       placeholder="Enter email">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input name="password" type="password" class="form-control"
+                                       id="exampleInputPassword1"
+                                       placeholder="Password">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="photo">Avatar</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input name="photo" type="file"
+                                               id="photo">
                                     </div>
-                                </div>
 
+                                </div>
                             </div>
                             <!-- /.card-body -->
 
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="row justify-content-between">
+                                    <div class="demo-inline-spacing">
+                                        <button type="button" class="btn btn-success">Save & Close</button>
+                                    </div>
+                                    <div class="demo-inline-spacing">
+                                        <button type="submit" value="submit" class="btn btn-primary">Save</button>
+                                        @if($data->prev())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->prev()) }}" class="btn btn-secondary">Pervious</a>
+                                        @endif
+                                        @if($data->next())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->next()) }}" class="btn btn-warning">Next</a>
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </section>
