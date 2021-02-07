7c7f79284e0bd5e7601c3996529249913a3e74c5
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 07:55:43 +0200

    edit new template : admins view { list , edit & create }

--- resources/views/admin/admins/form.blade.php
+++ resources/views/admin/admins/form.blade.php
@@ -1,34 +1,21 @@
-@extends('admin.layouts.master')
+@extends('admin/layouts/contentLayoutMaster')
+
+@section('title', isset($data)? 'Edit Admin' : 'New Admin')
+
+@section('vendor-style')
+@stop
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{isset($data)? 'Edit Admin '.$data->id : 'New Admin'}}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('admins.index')}}">Admins</a></li>
-                        <li class="breadcrumb-item active">{{isset($data)? 'Edit Admin' : 'New Admin'}}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-primary">
-                        @if(session('status'))
-                            <div class="alert alert-success alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-                                </button>
-                                <h5><i class="icon fas fa-check"></i> Success!</h5>
-                                {{ session('status') }}
-                            </div>
-                        @endif
+
+    <section id="multiple-column-form">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{isset($data)? 'Edit Admin '.$data->id : 'New Admin'}}</h4>
+                    </div>
+                    <div class="card-body">
+
                         <form method="post" enctype="multipart/form-data"
                               action="{{isset($data)? route('admins.update',$data->id):route('admins.store')}}"
                               role="form">
@@ -78,13 +65,21 @@
                             <!-- /.card-body -->
 
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="row justify-content-between">
+                                    <div class="demo-inline-spacing">
+                                        <button type="button" class="btn btn-success">Save & Close</button>
+                                    </div>
+                                    <div class="demo-inline-spacing">
+                                        <button type="submit" value="submit" class="btn btn-primary">Save</button>
+                                        <button type="button" class="btn btn-secondary">Pervious</button>
+                                        <button type="button" class="btn btn-warning">Next</button>
+                                    </div>
+                                </div>
                             </div>
                         </form>
+
                     </div>
-
                 </div>
-
             </div>
         </div>
     </section>
