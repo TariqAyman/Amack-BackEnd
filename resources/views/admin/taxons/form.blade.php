a4ce49d505e8d7c986c796696cba425add6f4012
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 08:01:47 +0200

    edit new template : Taxon view { list , edit & create }

--- resources/views/admin/taxons/form.blade.php
+++ resources/views/admin/taxons/form.blade.php
@@ -1,73 +1,55 @@
-@extends('admin.layouts.master')
+@extends('admin/layouts/contentLayoutMaster')
+
+@section('title', isset($data)? 'Edit Taxon' : 'New Taxon')
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{isset($data)? 'Edit Taxon '.$data->id : 'New Taxon'}}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('taxons.index')}}">Taxons</a></li>
-                        <li class="breadcrumb-item active">{{isset($data)? 'Edit Taxon' : 'New Taxon'}}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-primary">
-                        @if(session('status'))
-                            <div class="alert alert-success alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-                                </button>
-                                <h5><i class="icon fas fa-check"></i> Success!</h5>
-                                {{ session('status') }}
-                            </div>
-                        @endif
+    <section id="multiple-column-form">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{isset($data)? 'Edit Taxon '.$data->id : 'New Taxon'}}</h4>
+                    </div>
+                    <div class="card-body">
                         <form method="post" enctype="multipart/form-data"
                               action="{{isset($data)? route('taxons.update',$data->id):route('taxons.store')}}"
                               role="form">
                             @if(isset($data))
                                 @method('PUT')
                             @endif
-                            <div class="card-body">
-                                @csrf
-                                <div class="form-group">
-                                    <label for="name">Name</label>
-                                    <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
-                                           class="form-control" id="name"
-                                           placeholder="name">
-                                </div>
-                                <div class="form-group">
-                                    <label for="description">Description</label>
-                                    <textarea name="description"
-                                              class="form-control" id="description"
-                                              placeholder="Description"> @if(isset($data)){{$data->description}} @endif</textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="photo">Photo</label>
-                                    <input id="photo" type="file" class="form-control" name="photo" placeholder="photo">
-                                    <br>
-                                    <div style="display:inline-block">
-                                        @if(isset($data) && !empty($data->photo))
-                                            <div style="display:inline-block" class="img-wrap"
-                                                 id="photo-wrap">
-                                                <button id="delete-image"
-                                                        onclick="removePhoto(); return false;"
-                                                        class="close">
-                                                    &times;
-                                                </button>
-                                                <img id="photo-img" class="thumb" src="{{ $data->photo }}" alt="photo">
-                                            </div>
-                                        @endif
-                                    </div>
+
+                            @csrf
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
+                                       class="form-control" id="name"
+                                       placeholder="name">
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Description</label>
+                                <textarea name="description"
+                                          class="form-control" id="description"
+                                          placeholder="Description"> @if(isset($data)){{$data->description}} @endif</textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="photo">Photo</label>
+                                <input id="photo" type="file" class="form-control" name="photo" placeholder="photo">
+                                <br>
+                                <div style="display:inline-block">
+                                    @if(isset($data) && !empty($data->photo))
+                                        <div style="display:inline-block" class="img-wrap"
+                                             id="photo-wrap">
+                                            <button id="delete-image"
+                                                    onclick="removePhoto(); return false;"
+                                                    class="close">
+                                                &times;
+                                            </button>
+                                            <img id="photo-img" class="thumb" src="{{ $data->photo }}" alt="photo">
+                                        </div>
+                                    @endif
                                 </div>
                             </div>
+
                             <!-- /.card-body -->
 
                             <div class="card-footer">
@@ -75,14 +57,12 @@
                             </div>
                         </form>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </section>
 @endsection
-@section('scripts')
+@section('page-script')
     <script>
         function removePhoto() {
             $.ajax({
@@ -101,7 +81,7 @@
         }
     </script>
 @stop
-@section('styles')
+@section('page-style')
     <style>
         .thumb {
             max-width: 150px;
