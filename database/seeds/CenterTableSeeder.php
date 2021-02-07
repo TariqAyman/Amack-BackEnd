2fa03b66edd2d4855a66e36e1e76c4c94a9f09cd
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 08:04:56 +0200

    edit center seeder

--- database/seeds/CenterTableSeeder.php
+++ database/seeds/CenterTableSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\Center;
+use App\Models\DiveActivity;
 use App\Models\Staff;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class CenterTableSeeder extends Seeder
@@ -15,43 +17,56 @@ class CenterTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Factory::create('ar_EG');
+
         Center::firstOrCreate(['email' => 'q5z@live.com'], [
-            'name' => 'Test center',
+            'name' => $faker->name,
             'type' => 'center',
             'premises' => 'standalone',
-            'activity_id' => '1',
-            'mobile' => '01003003200',
-            'landline' => '123',
-            'website' => 'https://www.facebook.com/',
+            'activity_id' => DiveActivity::all()->random()->first()->id,
+            'mobile' => $faker->phoneNumber,
+            'landline' => $faker->phoneNumber,
+            'website' => $faker->url,
             'logo' => 'centers/1/hOJXLoICNpHwCwYRndUG26vlZicS0t1GKsMpKHyi.jpg',
             'stuff_members' => '1-4',
-            'address_1' => '6 october',
-            'address_2' => 'dfdsfds',
-            'center_lat' => '123',
-            'center_lng' => '321',
-            'manager_name' => 'dqwd',
-            'manager_mobile' => '+201091923922',
-            'owner_name' => 'dqwd',
-            'owner_mobile' => '+201091923922',
+            'address_1' => $faker->address,
+            'address_2' => $faker->address,
+            'center_lat' => $faker->latitude,
+            'center_lng' => $faker->longitude,
+            'manager_name' => $faker->name,
+            'manager_mobile' => $faker->phoneNumber,
+            'owner_name' => $faker->name,
+            'owner_mobile' => $faker->phoneNumber,
             'full_day' => 0,
-            'amenities' => '{\"pool\": {\"value\": null, \"enable\": \"on\"}, \"wifi\": {\"value\": null}, \"near_beach\": {\"value\": null}, \"pets_allowed\": {\"value\": null}, \"hotel_transport\": {\"value\": null}, \"orientation_dive\": {\"value\": null}, \"airport_transport\": {\"value\": null}}',
-            'languages' => '[\"en\", \"ru\"]',
-            'max_divers_per_trip' => 1,
-            'max_divers_per_day' => 2,
-            'max_day_divers' => 2,
-            'max_night_dives' => 2,
-            'max_em_dives' => NULL,
-            'mini_days_shore_dives' => 2,
-            'mini_days_boat_dives' => 2,
-            'max_days_shore_dives' => 2,
-            'max_days_boat_dives' => 2,
-            'mini_days_em_dives' => 2,
-            'mini_days_night_dives' => 2,
-            'max_days_em_dives' => 2,
-            'max_days_night_dives' => 2,
-            'bank_name' => 'fwefewfe12222222222',
-            'account_name' => 'fwefewf',
-            'account_number' => 'ث12ث12ثث',
+            'amenities' => [
+                "pool" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "wifi" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "near_beach" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "restaurant" => ["enable" => "on"],
+                "food_allowed" => ["enable" => "on"],
+                "pets_allowed" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "hotel_transport" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "orientation_dive" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "airport_transport" => ["value" => $faker->numerify("###"), "enable" => "on"],
+                "air_con_classrooms" => ["enable" => "on"]
+            ],
+            'languages' => ["en", "ru"],
+            'max_divers_per_trip' => $faker->numerify('##'),
+            'max_divers_per_day' => $faker->numerify('##'),
+            'max_day_divers' => $faker->numerify('##'),
+            'max_night_dives' => $faker->numerify('##'),
+            'max_em_dives' => $faker->numerify('##'),
+            'mini_days_shore_dives' => $faker->numerify('##'),
+            'mini_days_boat_dives' => $faker->numerify('##'),
+            'max_days_shore_dives' => $faker->numerify('##'),
+            'max_days_boat_dives' => $faker->numerify('##'),
+            'mini_days_em_dives' => $faker->numerify('##'),
+            'mini_days_night_dives' => $faker->numerify('##'),
+            'max_days_em_dives' => $faker->numerify('##'),
+            'max_days_night_dives' => $faker->numerify('##'),
+            'bank_name' => $faker->name,
+            'account_name' => $faker->name,
+            'account_number' => $faker->bankAccountNumber,
             'enabled' => 1,
         ]);
 
