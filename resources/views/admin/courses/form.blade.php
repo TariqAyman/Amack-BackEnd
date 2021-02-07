685130ab5c86467977005358cc021e33d30efb98
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 07:57:04 +0200

    edit new template : course view { list , edit & create }

--- resources/views/admin/courses/form.blade.php
+++ resources/views/admin/courses/form.blade.php
@@ -1,34 +1,16 @@
-@extends('admin.layouts.master')
+@extends('admin/layouts/contentLayoutMaster')
+
+@section('title', isset($data)? 'Edit Course' : 'New Course')
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{isset($data)? 'Edit Course '.$data->id : 'New Course'}}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('courses.index')}}">Courses</a></li>
-                        <li class="breadcrumb-item active">{{isset($data)? 'Edit Course' : 'New Course'}}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-primary">
-                        @if(session('status'))
-                            <div class="alert alert-success alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-                                </button>
-                                <h5><i class="icon fas fa-check"></i> Success!</h5>
-                                {{ session('status') }}
-                            </div>
-                        @endif
+    <section id="multiple-column-form">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{isset($data)? 'Edit Course '.$data->id : 'New Course'}}</h4>
+                    </div>
+                    <div class="card-body">
                         <form method="post" enctype="multipart/form-data"
                               action="{{isset($data)? route('courses.update',$data->id):route('courses.store')}}"
                               role="form">
@@ -114,13 +96,24 @@
                             <!-- /.card-body -->
 
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="row justify-content-between">
+                                    <div class="demo-inline-spacing">
+                                        <button type="button" class="btn btn-success">Save & Close</button>
+                                    </div>
+                                    <div class="demo-inline-spacing">
+                                        <button type="submit" value="submit" class="btn btn-primary">Save</button>
+                                        @if($data->prev())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->prev()) }}" class="btn btn-secondary">Pervious</a>
+                                        @endif
+                                        @if($data->next())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->next()) }}" class="btn btn-warning">Next</a>
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </section>
