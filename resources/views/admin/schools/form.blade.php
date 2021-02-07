5f803bb27fcc16da763c759087f6b070ae1676ce
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 08:01:14 +0200

    edit new template : School view { list , edit & create }

--- resources/views/admin/schools/form.blade.php
+++ resources/views/admin/schools/form.blade.php
@@ -1,93 +1,86 @@
-@extends('admin.layouts.master')
+@extends('admin/layouts/contentLayoutMaster')
+
+@section('title', isset($data)? 'Edit School' : 'New School')
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{isset($data)? 'Edit School '.$data->id : 'New School'}}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('schools.index')}}">Schools</a></li>
-                        <li class="breadcrumb-item active">{{isset($data)? 'Edit School' : 'New School'}}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-primary">
-                        @if(session('status'))
-                            <div class="alert alert-success alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-                                </button>
-                                <h5><i class="icon fas fa-check"></i> Success!</h5>
-                                {{ session('status') }}
-                            </div>
-                        @endif
+    <section id="multiple-column-form">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{isset($data)? 'Edit School'.$data->id : 'New School'}}</h4>
+                    </div>
+                    <div class="card-body">
                         <form method="post" enctype="multipart/form-data"
                               action="{{isset($data)? route('schools.update',$data->id):route('schools.store')}}"
                               role="form">
                             @if(isset($data))
                                 @method('PUT')
                             @endif
-                            <div class="card-body">
-                                @csrf
-                                <div class="form-group col-md-6">
-                                    <label for="name">Name</label>
-                                    <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
-                                           class="form-control" id="name"
-                                           placeholder="name">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="logo">Logo</label>
-                                    <input id="logo" type="file" class="form-control" name="logo" placeholder="logo"
-                                    >
-                                    <br>
-                                    <div style="display:inline-block">
-                                        @if(isset($data) && !empty($data->logo))
-                                            <div style="display:inline-block" class="img-wrap"
-                                                 id="logo-wrap">
-                                                <button id="delete-image"
-                                                        onclick="removeLogo(); return false;"
-                                                        class="close">
-                                                    &times;
-                                                </button>
-                                                <img id="logo-img" class="thumb" src="{{ $data->logo }}" alt="logo">
-                                            </div>
-                                        @endif
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="enabled">Enabled</label>
-                                        <div class="bootstrap-switch-square">
-                                            <input type="hidden" name="enabled" value="0">
-                                            <input type="checkbox" data-toggle="toggle" data-width="100" name="enabled"
-                                                   id="enabled" @if(isset($data) && $data->enabled) checked
-                                                   @endif value="1"/>
-                                        </div>
-                                    </div>
-                                </div>
 
+                            @csrf
+                            <div class="form-group col-md-6">
+                                <label for="name">Name</label>
+                                <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
+                                       class="form-control" id="name"
+                                       placeholder="name">
                             </div>
+                            <div class="form-group col-md-6">
+                                <label for="logo">Logo</label>
+                                <input id="logo" type="file" class="form-control" name="logo" placeholder="logo"
+                                >
+                                <br>
+                                <div style="display:inline-block">
+                                    @if(isset($data) && !empty($data->logo))
+                                        <div style="display:inline-block" class="img-wrap"
+                                             id="logo-wrap">
+                                            <button id="delete-image"
+                                                    onclick="removeLogo(); return false;"
+                                                    class="close">
+                                                &times;
+                                            </button>
+                                            <img id="logo-img" class="thumb" src="{{ $data->logo }}" alt="logo">
+                                        </div>
+                                    @endif
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="enabled">Enabled</label>
+                                    <div class="bootstrap-switch-square">
+                                        <input type="hidden" name="enabled" value="0">
+                                        <input type="checkbox" data-toggle="toggle" data-width="100" name="enabled"
+                                               id="enabled" @if(isset($data) && $data->enabled) checked
+                                               @endif value="1"/>
+                                    </div>
+                                </div>
+                            </div>
+
+
                             <!-- /.card-body -->
 
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="row justify-content-between">
+                                    <div class="demo-inline-spacing">
+                                        <button type="button" class="btn btn-success">Save & Close</button>
+                                    </div>
+                                    <div class="demo-inline-spacing">
+                                        <button type="submit" value="submit" class="btn btn-primary">Save</button>
+                                        @if($data->prev())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->prev()) }}" class="btn btn-secondary">Pervious</a>
+                                        @endif
+                                        @if($data->next())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->next()) }}" class="btn btn-warning">Next</a>
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </section>
 @endsection
-@section('scripts')
+@section('page-script')
     <script>
         function removeLogo() {
             $.ajax({
@@ -106,7 +99,7 @@
         }
     </script>
 @stop
-@section('styles')
+@section('page-style')
     <style>
         .thumb {
             max-width: 150px;
