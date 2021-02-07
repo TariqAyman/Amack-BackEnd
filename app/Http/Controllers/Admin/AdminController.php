69beefecea42712c84aaa778fdd54f4280e1dc21
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 08:05:49 +0200

    edit AdminController for use breadcrumbs

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Repositories\Repository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\View\View;
 
 abstract class AdminController extends Controller
@@ -16,6 +17,24 @@ abstract class AdminController extends Controller
 
     protected $block;
 
+    /**
+     * @var array
+     */
+    protected $indexBreadcrumbs = [];
+
+    /**
+     * @var array
+     */
+    protected $editBreadcrumbs;
+
+    /**
+     * @var array
+     */
+    protected $createBreadcrumbs;
+
+    /**
+     * AdminController constructor.
+     */
     public function __construct()
     {
         $current = $this->block;
@@ -23,44 +42,76 @@ abstract class AdminController extends Controller
         $this->repository = app($this->repository);
     }
 
-    public function index(): View
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|View
+     */
+    public function index()
     {
-        return view('admin.' . $this->block . '.index');
+        return view('admin.' . $this->block . '.index', ['breadcrumbs' => $this->indexBreadcrumbs]);
     }
 
+    /**
+     * @param int $id
+     * @return \Illuminate\Database\Eloquent\Model
+     */
     public function show(int $id)
     {
         return $this->repository->find($id);
     }
 
-    public function edit(int $id): View
+    /**
+     * @param int $id
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|View
+     */
+    public function edit(int $id)
     {
         $data = $this->repository->find($id);
-        return view('admin.' . $this->block . '.form', compact('data'));
+        $this->editBreadcrumbs = array_merge($this->indexBreadcrumbs, [['name' => "Edit #{$data->id}"]]);
+        return view('admin.' . $this->block . '.form', ['data' => $data, 'breadcrumbs' => $this->editBreadcrumbs]);
     }
 
-    public function create(): View
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|View
+     */
+    public function create()
     {
-        return view('admin.' . $this->block . '.form');
+        $this->createBreadcrumbs = array_merge($this->indexBreadcrumbs, [['name' => "Create New"]]);
+        return view('admin.' . $this->block . '.form', ['breadcrumbs' => $this->createBreadcrumbs]);
     }
 
+    /**
+     * @return mixed
+     */
     public function data()
     {
         return $this->repository->getDatatable();
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function store(Request $request)
     {
         $this->repository->insert($request->all());
         return redirect(route($this->block . '.index'))->with('status', 'Added Successfully!');
     }
 
+    /**
+     * @param int $id
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function update(int $id, Request $request)
     {
         $this->repository->update($id, $request->all());
         return redirect(route($this->block . '.edit', $id))->with('status', 'Updated Successfully!');
     }
 
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function destroy(int $id)
     {
         $this->repository->delete($id);
