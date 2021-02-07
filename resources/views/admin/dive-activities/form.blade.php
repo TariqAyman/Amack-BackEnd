fbcc35b7c5db780d270413d3e4e4611537297259
Tariq Ayman <tariq.ayman94@gmail.com>
2021-02-07 07:58:05 +0200

    edit new template : dive activities view { list , edit & create }

--- resources/views/admin/dive-activities/form.blade.php
+++ resources/views/admin/dive-activities/form.blade.php
@@ -1,92 +1,84 @@
-@extends('admin.layouts.master')
+@extends('admin/layouts/contentLayoutMaster')
+
+@section('title', isset($data)? 'Edit Dive Activities' : 'New Dive Activities')
+
 @section('content')
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>{{isset($data)? 'Edit Dive Activity '.$data->id : 'New Dive Activity'}}</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('dive-activities.index')}}">Dive Activities</a>
-                        </li>
-                        <li class="breadcrumb-item active">{{isset($data)? 'Edit Dive Activity' : 'New Dive Activity'}}</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card card-primary">
-                        @if(session('status'))
-                            <div class="alert alert-success alert-dismissible">
-                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;
-                                </button>
-                                <h5><i class="icon fas fa-check"></i> Success!</h5>
-                                {{ session('status') }}
-                            </div>
-                        @endif
+    <section id="multiple-column-form">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{isset($data)? 'Edit Dive Activity' : 'New Dive Activity'}}</h4>
+                    </div>
+                    <div class="card-body">
                         <form method="post" enctype="multipart/form-data"
                               action="{{isset($data)? route('dive-activities.update',$data->id):route('dive-activities.store')}}"
                               role="form">
                             @if(isset($data))
                                 @method('PUT')
                             @endif
-                            <div class="card-body">
-                                @csrf
-                                <div class="form-group col-md-6">
-                                    <label for="name">Name</label>
-                                    <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
-                                           class="form-control" id="name"
-                                           placeholder="name">
-                                </div>
-                                <div class="form-group col-md-6">
-                                    <label for="description">Description</label>
-                                    <textarea name="description"
-                                              class="form-control" id="description"
-                                              placeholder="Description"> @if(isset($data)){{$data->description}} @endif</textarea>
-                                </div>
 
-                                <div class="form-group col-md-6">
-                                    <label for="photo">Photo</label>
-                                    <input id="photo" type="file" class="form-control" name="icon" placeholder="photo"
-                                    >
-                                    <br>
-                                    <div style="display:inline-block">
-                                        @if(isset($data) && !empty($data->icon))
-                                            <div style="display:inline-block" class="img-wrap"
-                                                 id="photo-wrap">
-                                                <button id="delete-image"
-                                                        onclick="removePhoto(); return false;"
-                                                        class="close">
-                                                    &times;
-                                                </button>
-                                                <img id="photo-img" class="thumb" src="{{ $data->icon }}" alt="photo">
-                                            </div>
-                                        @endif
-                                    </div>
-                                </div>
-
+                            @csrf
+                            <div class="form-group col-md-6">
+                                <label for="name">Name</label>
+                                <input @if(isset($data)) value="{{$data->name}}" @endif name="name" type="text"
+                                       class="form-control" id="name"
+                                       placeholder="name">
                             </div>
+                            <div class="form-group col-md-6">
+                                <label for="description">Description</label>
+                                <textarea name="description"
+                                          class="form-control" id="description"
+                                          placeholder="Description"> @if(isset($data)){{$data->description}} @endif</textarea>
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="photo">Photo</label>
+                                <input id="photo" type="file" class="form-control" name="icon" placeholder="photo"
+                                >
+                                <br>
+                                <div style="display:inline-block">
+                                    @if(isset($data) && !empty($data->icon))
+                                        <div style="display:inline-block" class="img-wrap"
+                                             id="photo-wrap">
+                                            <button id="delete-image"
+                                                    onclick="removePhoto(); return false;"
+                                                    class="close">
+                                                &times;
+                                            </button>
+                                            <img id="photo-img" class="thumb" src="{{ $data->icon }}" alt="photo">
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+
+
                             <!-- /.card-body -->
 
                             <div class="card-footer">
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <div class="row justify-content-between">
+                                    <div class="demo-inline-spacing">
+                                        <button type="button" class="btn btn-success">Save & Close</button>
+                                    </div>
+                                    <div class="demo-inline-spacing">
+                                        <button type="submit" value="submit" class="btn btn-primary">Save</button>
+                                        @if($data->prev())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->prev()) }}" class="btn btn-secondary">Pervious</a>
+                                        @endif
+                                        @if($data->next())
+                                            <a type="button" href="{{ route('dive-sites.edit',$data->next()) }}" class="btn btn-warning">Next</a>
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
-
                 </div>
-
             </div>
         </div>
     </section>
 @endsection
-@section('scripts')
+@section('page-script')
     <script>
         function removePhoto() {
             $.ajax({
@@ -105,7 +97,7 @@
         }
     </script>
 @stop
-@section('styles')
+@section('page-style')
     <style>
         .thumb {
             max-width: 150px;
